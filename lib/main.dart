8c349c877b2667ceda92fdc0fbab9bba7171062d
Steve Moreau <stevemoreau@users.noreply.github.com>
2019-12-17 15:11:59 +0100

    [A] Simple example for navigation delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,84 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
 import 'dart:async';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:webview/screens/HomeScreen.dart';
-import 'package:webview/utils/i18n/bloc_provider_new.dart';
-import 'package:webview/utils/i18n/multiling_bloc.dart';
-import 'package:webview/utils/i18n/multiling_global_translations.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-Future main() async {
-  //await allTranslations.init();
+void main() => runApp(MaterialApp(home: WebViewExample()));
 
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.dumpErrorToConsole(details);
-  };
-
-  runApp(MyApp());
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TranslationsBloc translationsBloc;
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-  _MyAppState();
-
-  @override
-  void initState() {
-    super.initState();
-    translationsBloc = TranslationsBloc();
-  }
-
-  @override
-  void dispose() {
-    translationsBloc?.dispose();
-    super.dispose();
-  }
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
-    Widget defaultHome = HomeScreen();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView example'),
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'https://www.moneyvox.fr/energie/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
 
-    return BlocProvider<TranslationsBloc>(
-        bloc: translationsBloc,
-        child: StreamBuilder<Locale>(
-            stream: translationsBloc.currentLocale,
-            initialData: allTranslations.locale,
-            builder: (BuildContext context, AsyncSnapshot<Locale> snapshot) {
-              return MaterialApp(
-                  navigatorObservers: <NavigatorObserver>[],
-                  title: allTranslations.text("webview"),
-                  theme: ThemeData(fontFamily: 'Regular'),
-                  home: defaultHome);
-            }));
+          javascriptChannels: <JavascriptChannel>[
+            _toasterJavascriptChannel(context),
+          ].toSet(),
+          navigationDelegate: (NavigationRequest request) {
+
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          }
+        );
+      }),
+      floatingActionButton: favoriteButton(),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  Widget favoriteButton() {
+    return FutureBuilder<WebViewController>(
+        future: _controller.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return FloatingActionButton(
+              onPressed: () async {
+                final String url = await controller.data.currentUrl();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(content: Text('Favorited $url')),
+                );
+              },
+              child: const Icon(Icons.favorite),
+            );
+          }
+          return Container();
+        });
   }
 }
--- lib/values/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyColors {
-  static const seaweed_green = const Color(0xff3bb05e);
-  static const blue = const Color(0xff0053c2);
-  static const white = const Color(0xffffffff);
-  static const black = const Color(0xff212121);
-  static const pale_grey = const Color(0xffecf2fa);
-}
